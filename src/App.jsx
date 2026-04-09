b8cadad6a7481f2025dc0e37cc0e44ad973b9baf
jellomoon <damonita@gmail.com>
2026-04-09 11:08:59 +0200

    Add basic dictionary search form scaffold

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Hello World</h1>
-      </header>
+      <div>
+        <Dictionary />
+        <footer>
+          Coded with 🤍 by Mona Beyersdorf, open-sourced on{" "}
+          <a href="https://github.com/jellomoon/dictionary-app">GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
